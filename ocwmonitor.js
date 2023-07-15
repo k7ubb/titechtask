711b4a068b7d5cdc3d69052efe9eda827edd3bf0
k7ubb <kubota.sou@vega.ocn.ne.jp>
2023-07-16 00:31:36 +0900

    ver 2.0.0 (20220403)
    OCW-iのサービス終了に対応
    moodleのAPIを仕様するように変更

--- ocwmonitor.js
+++
@@ -1,4 +0,0 @@
-if(document.getElementsByClassName("alartBox").length &&
-   document.getElementsByClassName("alartBox")[0].innerText=="課題の提出が完了しました。"){
-	chrome.runtime.sendMessage("load");
-}
--- t2monitor.js
+++
@@ -1,4 +0,0 @@
-if(document.referrer.indexOf("action=editsubmission") != -1 &&
-   document.getElementsByClassName("submissionstatussubmitted").length){
-	chrome.runtime.sendMessage("load");
-}
