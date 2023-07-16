07478ddcb4ff7476b07a2855b531460a0cb3a450
k7ubb <kubota.sou@vega.ocn.ne.jp>
2023-07-16 09:27:07 +0900

    ver 3.0.0 (20230716)
    titechcalの機能を統合
    レイアウトの調整

---
+++ popup/api.js
@@ -0,0 +1,75 @@
+// t2scholaのtoken文字列を返す
+// portalへのログインが必要な場合、空文字列を返す
+function getToken(){
+	let xhr = new XMLHttpRequest();
+	xhr.open("get", "https://t2schola.titech.ac.jp/admin/tool/mobile/launch.php?service=moodle_mobile_app&passport=14029&urlscheme=mmt2schola", false);
+	xhr.send();
+	if(xhr.responseURL.indexOf("portal.nap.gsic.titech.ac.jp") == -1){
+		let token_unformat = xhr.responseText.match(/"mmt2schola\:\/\/token=(.*)"/)[1];
+		let token = atob(token_unformat).split(":::")[1];
+		chrome.storage.local.set({"token": token}, function(){});
+		return token;
+	}
+	else{
+		return "";
+	}
+}
+
+
+// t2scholaのuserID文字列を返す
+// token読み直しが必要な場合、空文字列を返す
+function getUserID(token){
+	let xhr = new XMLHttpRequest();
+	xhr.open("post", "https://t2schola.titech.ac.jp/webservice/rest/server.php?moodlewsrestformat=json", false);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send("wsfunction=core_webservice_get_site_info&wstoken=" + token);
+	let result_json = JSON.parse(xhr.responseText);
+	if(result_json.errorcode == "invalidtoken"){
+		return "";
+	}
+	else{
+		return String(result_json.userid);
+	}
+}
+
+
+// mod_assign_get_assignmentsの結果のJSONを返す
+function getAssignments(token){
+	let xhr = new XMLHttpRequest();
+	xhr.open("post", "https://t2schola.titech.ac.jp/webservice/rest/server.php?moodlewsrestformat=json", false);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send("wsfunction=mod_assign_get_assignments&wstoken=" + token);
+	return JSON.parse(xhr.responseText);
+}
+
+
+// mod_quiz_get_quizzes_by_coursesの結果のJSONを返す
+function getQuizzes(token){
+	let xhr = new XMLHttpRequest();
+	xhr.open("post", "https://t2schola.titech.ac.jp/webservice/rest/server.php?moodlewsrestformat=json", false);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send("wsfunction=mod_quiz_get_quizzes_by_courses&wstoken=" + token);
+	return JSON.parse(xhr.responseText);
+}
+
+
+// mod_workshop_get_workshops_by_coursesの結果のJSONを返す
+function getWorkshops(token){
+	let xhr = new XMLHttpRequest();
+	xhr.open("post", "https://t2schola.titech.ac.jp/webservice/rest/server.php?moodlewsrestformat=json", false);
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send("wsfunction=mod_workshop_get_workshops_by_courses&wstoken=" + token);
+	return JSON.parse(xhr.responseText);
+}
+
+
+// t2scholaの課題の提出状況を返す (非同期通信, 読み込み後にonloadfunctionを実行)
+function getAssignSubmission(token, userid, assignmentid, onloadfunction){
+	let xhr = new XMLHttpRequest();
+	xhr.open("post", "https://t2schola.titech.ac.jp/webservice/rest/server.php?moodlewsrestformat=json");
+	xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+	xhr.send("wsfunction=mod_assign_get_submission_status&userid=" + userid + "&assignid=" + assignmentid + "&wstoken=" + token);
+	xhr.onloadend = function(){
+		onloadfunction(JSON.parse(xhr.responseText));
+	};
+}
---
+++ popup/calender.js
@@ -0,0 +1,121 @@
+function posCalender(course, calender){
+	for(let i=0; i<5; i++){
+	for(let j=0; j<5; j++){
+		if(calender[i][j] && calender[i][j].id == course.id){ return [i, j]; }
+	}
+	}
+	return [-1, -1];
+}
+
+
+function mousePosCalender(event){
+	let x = event.pageX;
+	let y = event.pageY;
+	if(45 < x && x < 490 && 33 < y && y < 340){
+		return [Math.floor((x - 45) / 90), Math.floor((y - 33) / 62)];
+	}
+	else if(5 < x && x < 500 && 350 < y && y < 400){
+		return [-1, -1];
+	}
+}
+
+
+function createCalenderNode(course, calender){
+	let div = document.createElement("div");
+	div.innerHTML = course.name[0];
+	div.className = "course";
+	div.setAttribute("id", course.id);
+	div.draggable = true;
+	div.ondrag = function(){
+		if(mousePosCalender(event)){
+			let [x, y] = mousePosCalender(event);
+			let e = (x == -1)? document.getElementById("calender_courses") : document.getElementsByTagName("td")[x + y*5].firstChild;
+			if(e && document.getElementsByClassName("dragging")[0] && e && e != document.getElementsByClassName("dragging")[0]){
+				document.getElementsByClassName("dragging")[0].className = "dragtarget";
+			}
+			if(e.className == "dragtarget"){
+				e.className = "dragging";
+			}
+		}
+	};
+	div.ondragend = function(){
+		let [i, j] = posCalender(course, calender);
+		if(mousePosCalender(event)){
+			let [x, y] = mousePosCalender(event);
+			if(x == -1 && y == -1){
+				if(i != -1 && j != -1){
+					calender[i][j] = null;
+					chrome.storage.local.set({"calender": JSON.stringify(calender)}, drawCalender);
+				}
+			}
+			else{
+				if(!calender[x][y]){
+					if(i != -1 && j != -1){
+						calender[i][j] = null;
+					}
+					calender[x][y] = course;
+					chrome.storage.local.set({"calender": JSON.stringify(calender)}, drawCalender);
+					drawCalender();
+				}
+				div.display = "none";
+			}
+		}
+		if(document.getElementsByClassName("dragging")[0]){
+			document.getElementsByClassName("dragging")[0].className = "dragtarget";
+		}
+	};
+	div.onclick = function(){
+		chrome.tabs.create({"url": "https://t2schola.titech.ac.jp/course/view.php?id=" + course.id});
+	};
+	
+	return div;
+}
+
+
+function createCalenderEmptyNode(i, j){
+	let div = document.createElement("div");
+	div.className = "dragtarget";
+	div.setAttribute("x", i);
+	div.setAttribute("y", j);
+	return div;
+}
+
+
+function drawCalender(){
+	chrome.storage.local.get(["courses", "calender"], function(s){
+		courses    = JSON.parse(s.courses || "[]");
+		calender   = JSON.parse(s.calender || "[[],[],[],[],[]]");
+		
+		document.getElementById("calender_table").innerHTML = "";
+		document.getElementById("calender_courses").firstChild.innerHTML = "";
+		
+		for(let i=0; i<5; i++){
+			let tr = document.createElement("tr");
+			let th = document.createElement("th");
+			th.innerHTML = ["8:50 〜 10:30", "10:45<br>〜<br>12:25", "13:25 〜 15:25", "15:40 〜 17:20", "17:30 〜 19:10"][i];
+			tr.appendChild(th);
+			for(let j=0; j<5; j++){
+				let td = document.createElement("td");
+				if(calender[j][i]){
+					let node = createCalenderNode(calender[j][i], calender);
+					td.appendChild(node);
+				}
+				else{
+					let div = createCalenderEmptyNode(i, j);
+					td.appendChild(div);
+				}
+				tr.appendChild(td);
+			}
+			document.getElementById("calender_table").appendChild(tr);
+		}
+		
+		let count = 0;
+		for(let course of courses){
+			if(posCalender(course, calender)[0] != -1){ continue; }
+			count++;
+			let node = createCalenderNode(course, calender);
+			document.getElementById("calender_courses").firstChild.appendChild(node);
+		}
+		document.getElementById("calender_courses").firstChild.style.width = (104 * count) + "px";
+	});
+}
