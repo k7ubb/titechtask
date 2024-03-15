f160231cb2d911cc8c59bc76ee19fdaa1628ad8d
k7ubb <64680945+k7ubb@users.noreply.github.com>
2024-03-15 15:33:03 +0900

    提出状況の確認 (途中まで)

--- popup/script.js
+++ popup/script.js
@@ -2,27 +2,23 @@
 	T2Schola.token = (await chrome.storage.local.get("token")).token;
 	Data.courses = (await chrome.storage.local.get("courses")).courses || [];
 	Data.tasks = (await chrome.storage.local.get("tasks")).tasks || [];
+	Data.submission = (await chrome.storage.local.get("submission")).submission || [];
 	draw();
-/*
-	console.log(await T2Schola.wsfunction("mod_assign_get_submission_status", {
-		userid: 15637,
-		assignid: 24775,
-	}));
-	*/
 })();
 
 const draw = () => {
 	document.getElementById("message").innerHTML = "";
 	Data.tasks.map((task) => {
 		const p = document.createElement("p");
-		p.innerHTML = task.name;
+		p.innerHTML = (Data.submission.indexOf(task.id) !== -1? "済 " : "未 ") + task.name;
 		p.title = JSON.stringify(task).replace(/,/g, "\n");
 		document.getElementById("message").appendChild(p);
 	});
 };
 
 document.getElementById("upload").onclick = async () => {
-	await Data.t2ScholaUpdate();
+//	await Data.t2ScholaUpdate();
+	await Data.updateSubmission();
 	draw();
 };
 
